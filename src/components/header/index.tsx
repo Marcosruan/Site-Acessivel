75e48d27eeec3e14d43565e316163f87e3062f19
Marcos Ruan <113126971+Marcosruan@users.noreply.github.com>
2026-06-10 11:07:56 -0300

    Adicionar estilos da home

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,12 +9,13 @@ type HeaderProps = {
 
 export function Header({ titulo, area, setArea}: HeaderProps) {
   return (
-    <header className = "flex items-center gap-3 px-4 py-4 border-b border-gray-300">
+    <header className = "flex items-center width-full height-[132px] gap-3 px-4 py-4 border-b-2 border-[#2187FF] shadow-[0_5px_10px_rgba(4,97,207,0.50)]">
       <img
         src={logo}
         alt="Logo do site acessível"
         width="108px"
         height="108px"
+        className="ml-24"
       />
       <a onClick={() => setArea(area)}>
       <h1 className="font-semibold text-gray-700" >{titulo}</h1>
