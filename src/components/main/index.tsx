8b1e30689e5adf97b3d3d0b5fc6e8f7483729173
Carlos H <www.carlos4k@gmail.com>
2026-06-10 10:50:27 -0300

    docs/style: adicionar estilos da home, implementar o tailwind

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -30,11 +30,12 @@ export function Main({ area, conteudo, setArea, setConteudo }: MainProps) {
     <main className="p-8">
       <section>
         {" "}
-        <h2 className="text-center text-gray-700 mb-8">Matéria</h2>
+        <h2 className="text-center font-inter font-bold text-black text-2xl mb-4">Matérias</h2>
+        <p className="text-center text-[#5C5C5C] mb-8">A seguir veja as matérias disponíveis. <span className="text-[#0973F0]">Clique</span> em uma delas para saber mais sobre.</p>
         <SectionHome setArea={setArea} setConteudo={setConteudo} />
       </section>
       <section className="mt-16">
-        <h2 className="text-center text-gray-700 mb-6">Exercícios</h2>
+        <h2 className="text-center font-inter font-bold text-black text-2xl mb-6">Exercícios</h2>
 
         <article className="grid md:grid-cols-2 gap-6">
 
