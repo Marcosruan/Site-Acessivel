7afffd9c8f6682116ae7c0a454b58a614667f652
Marcos <marcos.figueiredo.dev@gmail.com>
2026-08-31 07:41:59 -0300

    refactor: deletar backend

--- backend/dist/prisma/client/models.d.ts
+++
@@ -1,5 +0,0 @@
-export type * from './models/Article.js';
-export type * from './models/Video.js';
-export type * from './models/Exercise.js';
-export type * from './models/Alternative.js';
-export type * from './commonInputTypes.js';
--- backend/dist/prisma/client/models/Exercise.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=Exercise.js.map
